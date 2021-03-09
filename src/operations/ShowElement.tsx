892503d413ea6ecfc5ff6463c1c7e93921e7ea75
richard1230 <yongjinhu1230@gmail.com>
2021-03-09 15:20:48 +0800

    SHowElement 有问题，排序还未搞定

--- src/operations/ShowElement.tsx
+++ src/operations/ShowElement.tsx
@@ -1,15 +1,42 @@
-import React,{ FormEvent, useContext, MouseEvent}  from 'react'
-import { AppContext } from '../Context';
+import React, {FormEvent, useContext, MouseEvent, useState} from 'react'
+import {AppContext, ContentType} from '../Context';
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const ShowElement:React.FC<any> = (props) =>{
 
     const { state: globalProps, dispatch } = useContext(AppContext);
     const { _elementContent: elementContent } = globalProps;
+    const [items,setitems] = useState<ContentType[]>([]);
 
+    //重新排序函数
+    const reorder = (list:any, startIndex:number, endIndex:number) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    };
+
+
+    const onDragEnd = (result:any) =>{
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const newitems = reorder(
+            items,
+            result.source.index,
+            result.destination.index
+        );
+        //
+        setitems(
+            newitems
+        );
+    }
     //文字编辑
     const editTxt = (e: FormEvent<HTMLParagraphElement>,id:string) => {
-        const txtItem = elementContent.find(e => e.id === id)
+        const txtItem = elementContent.find(e => (e.id === id))
         const newSate ={
             isDeleteShow:true,
             isTextEditShow:true,
@@ -62,39 +89,84 @@ const ShowElement:React.FC<any> = (props) =>{
     };
 
     return(
-        // <DragDropContext  onDragEnd={}>
         <div className='col-xs-6 text-left' style={{border:'1px solid',display:'block'}}>
             <h3>简单编辑器</h3>
             {
+                <DragDropContext  onDragEnd={onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {
+                            (provided )=>(
+                                <div
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                >
+                                    elementContent.map( (el, index) =>
+                                    (
+                                    <Draggable key={el.id} draggableId={el.id} index={index}>
+                                        {
+                                            (provided)=>(
+                                                <div
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                >
+                                                    {
 
-                    elementContent.map((el,index)=>{
-                    if(el.type ==="pic")
-                        return (
-                            <div key={el.id}
-                                 style={{border:el.isBorderShow?'1px solid':''}}
-                                 onMouseEnter={(e)=>{makeBorderShow(e,el.id)}}
-                                 onMouseLeave={(e)=>{hideBorderShow(e,el.id)}}>
-                                <img key={index}
-                                     src={el.content}
-                                     onClick={(e)=>{editPic(e,el.id)}}
-                                     alt="loading error"
-                                     className='img-responsive'/>
-                            </div>)
-                    else if(el.type ==="txt")
-                        return (
-                            <div key={el.id}
-                                 style={{border:el.isBorderShow?'1px solid':''}}
-                                 onMouseEnter={(e)=>{makeBorderShow(e,el.id)}}
-                                 onMouseLeave={(e)=>{hideBorderShow(e,el.id)}}>
-                                <p onClick={(e)=>{editTxt(e,el.id)}} >{el.content}</p>
-                            </div>)
-                })
+                                                        if (el.type === "pic")
+                                                        return (
+                                                        <div key={el.id}
+                                                        style={{border: el.isBorderShow ? '1px solid' : ''}}
+                                                        onMouseEnter={(e) => {
+                                                        makeBorderShow(e, el.id)
+                                                    }}
+                                                        onMouseLeave={(e) => {
+                                                        hideBorderShow(e, el.id)
+                                                    }}>
+                                                        <img key={index}
+                                                        src={el.content}
+                                                        onClick={(e) => {
+                                                        editPic(e, el.id)
+                                                    }}
+                                                        alt="loading error"
+                                                        className='img-responsive'/>
+                                                        </div>)
+                                                        else if (el.type === "txt")
+                                                        return (
+                                                        <div key={el.id}
+                                                        style={{border: el.isBorderShow ? '1px solid' : ''}}
+                                                        onMouseEnter={(e) => {
+                                                        makeBorderShow(e, el.id)
+                                                    }}
+                                                        onMouseLeave={(e) => {
+                                                        hideBorderShow(e, el.id)
+                                                    }}>
+                                                        <p onClick={(e) => {
+                                                        editTxt(e, el.id)
+                                                    }}>{el.content}</p>
+                                                        </div>)
+                                                    }
+
+
+                                                </div>
+                                            )
+                                        }
+
+                                    </Draggable>
+
+                                    )
+                                    )
+                                </div>
+                            )
+                        }
+                    </Droppable>
+                </DragDropContext>
+
             }
+
         </div>
-       // </DragDropContext>
 
     )
 
-            }
+}
 
-export default ShowElement
+export default ShowElement;
