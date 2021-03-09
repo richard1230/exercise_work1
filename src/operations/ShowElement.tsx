bc45c9f30c663ab65170f322a19a9a4cd55f9430
richard1230 <yongjinhu1230@gmail.com>
2021-03-09 17:18:43 +0800

    排序基本搞定

--- src/operations/ShowElement.tsx
+++ src/operations/ShowElement.tsx
@@ -1,22 +1,22 @@
-import React, {FormEvent, useContext, MouseEvent, useState,useEffect} from 'react'
+import React, {FormEvent, useContext, MouseEvent} from 'react'
 import {AppContext, ContentType} from '../Context';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 
-const ShowElement:React.FC<any> = (props) =>{
+const ShowElement: React.FC<any> = (props) => {
 
-    const { state: globalProps, dispatch } = useContext(AppContext);
-    const { _elementContent: elementContent } = globalProps;
+    const {state: globalProps, dispatch} = useContext(AppContext);
+    const {_elementContent: elementContent} = globalProps;
 
     //重新排序函数
-    const reorder = (list:any, startIndex:number, endIndex:number) => {
-        const result:ContentType[] = Array.from(list);
+    const reorder = (list: any, startIndex: number, endIndex: number) => {
+        const result: ContentType[] = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
         result.splice(endIndex, 0, removed);
         return result;
     };
 
 
-    const onDragEnd = (result:any) =>{
+    const onDragEnd = (result: any) => {
         // dropped outside the list
         if (!result.destination) {
             return;
@@ -29,21 +29,21 @@ const ShowElement:React.FC<any> = (props) =>{
         );
         dispatch(
             {
-                type:"updateElements",
-                payload:newitems
+                type: "updateElements",
+                payload: newitems
             }
         )
         //
     }
     //文字编辑
-    const editTxt = (e: FormEvent<HTMLParagraphElement>,id:string) => {
+    const editTxt = (e: FormEvent<HTMLParagraphElement>, id: string) => {
         const txtItem = elementContent.find(e => (e.id === id))
-        const newSate ={
-            isDeleteShow:true,
-            isTextEditShow:true,
-            isPicEditShow:false,
-            txtvalue:txtItem?.content,
-            idstate:id,
+        const newSate = {
+            isDeleteShow: true,
+            isTextEditShow: true,
+            isPicEditShow: false,
+            txtvalue: txtItem?.content,
+            idstate: id,
         }
         dispatch({
             type: 'editTxt',
@@ -52,14 +52,14 @@ const ShowElement:React.FC<any> = (props) =>{
     };
 
     //编辑图片
-    const editPic = (e: FormEvent<HTMLImageElement>,id:string) => {
+    const editPic = (e: FormEvent<HTMLImageElement>, id: string) => {
         const picItem = elementContent.find(e => e.id === id)
-        const newSate ={
-            isDeleteShow:true,
-            isPicEditShow:true,
-            isTextEditShow:false,
-            picvalue:picItem?.content,
-            idstate:id,
+        const newSate = {
+            isDeleteShow: true,
+            isPicEditShow: true,
+            isTextEditShow: false,
+            picvalue: picItem?.content,
+            idstate: id,
         }
         dispatch({
             type: 'editPic',
@@ -68,9 +68,9 @@ const ShowElement:React.FC<any> = (props) =>{
     };
 
     //showborder
-    const makeBorderShow = (e:MouseEvent<HTMLDivElement>,id:string) => {
+    const makeBorderShow = (e: MouseEvent<HTMLDivElement>, id: string) => {
         const element = elementContent.find(e => e.id === id)
-        if(element)
+        if (element)
             element.isBorderShow = true
         dispatch({
             type: 'makeBorderShow',
@@ -79,9 +79,9 @@ const ShowElement:React.FC<any> = (props) =>{
     };
 
     //hideborder
-    const hideBorderShow = (e:MouseEvent<HTMLDivElement>,id:string) => {
+    const hideBorderShow = (e: MouseEvent<HTMLDivElement>, id: string) => {
         const element = elementContent.find(e => e.id === id)
-        if(element)
+        if (element)
             element.isBorderShow = false
         dispatch({
             type: 'hideBorderShow',
@@ -89,11 +89,11 @@ const ShowElement:React.FC<any> = (props) =>{
         })
     };
 
-    const getElement = (el:ContentType)=>{
+    const getElement = (el: ContentType) => {
         if (el.type === "pic")
             return (
                 <div key={el.id}
-                     style={{ border: el.isBorderShow ? '1px solid' : '' }}
+                     style={{border: el.isBorderShow ? '1px solid' : ''}}
                      onMouseEnter={(e) => {
                          makeBorderShow(e, el.id)
                      }}
@@ -106,12 +106,12 @@ const ShowElement:React.FC<any> = (props) =>{
                             editPic(e, el.id)
                         }}
                         alt="loading error"
-                        className='img-responsive' />
+                        className='img-responsive'/>
                 </div>)
         else if (el.type === "txt")
             return (
                 <div key={el.id}
-                     style={{ border: el.isBorderShow ? '1px solid' : '' }}
+                     style={{border: el.isBorderShow ? '1px solid' : ''}}
                      onMouseEnter={(e) => {
                          makeBorderShow(e, el.id)
                      }}
@@ -124,36 +124,38 @@ const ShowElement:React.FC<any> = (props) =>{
                 </div>)
     }
 
-    return(
-        <div className='col-xs-6 text-left' style={{border:'1px solid',display:'block'}}>
+    return (
+        <div className='col-xs-6 text-left' style={{border: '1px solid', display: 'block'}}>
             <h3>简单编辑器</h3>
             {
-                <DragDropContext  onDragEnd={onDragEnd}>
+                <DragDropContext onDragEnd={onDragEnd}>
                     <Droppable droppableId="droppable">
                         {
-                            (provided:any )=>(
+                            (provided: any) => (
                                 <div
                                     {...provided.droppableProps}
                                     ref={provided.innerRef}
                                 >
-                                    elementContent.map((el, index) => <Draggable key={el.id} draggableId={el.id} index={index}>
-                                        {
-                                            (provided:any)=>(
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-                                                >
-                                                    {
-                                                        getElement(el)
-                                                    }
-                                                </div>
-                                            )
-                                        }
+                                    {
 
-                                    </Draggable>
+                                        elementContent.map((el, index) => <Draggable key={el.id} draggableId={el.id}
+                                                                                     index={index}>
+                                                {
+                                                    (provided: any) => (
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                        >
+                                                            {
+                                                                getElement(el)
+                                                            }
+                                                        </div>
+                                                    )
+                                                }
 
-                                    )
+                                            </Draggable>
+                                        )
                                     }
                                 </div>
                             )
